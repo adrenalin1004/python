5073abcfd2a06b1a6b6e2f78c1891fd89bdf9b70
adrenalin1004 <63244835+adrenalin1004@users.noreply.github.com>
2021-03-01 01:50:46 +0900

    Update Quiz-210301.py

--- PyQuiz/Quiz-210301.py
+++ PyQuiz/Quiz-210301.py
@@ -14,3 +14,18 @@ print('"C:\windows"')
 
 #5. \t와 \n의 역할을 설명, \n 줄바꿈, \t 탭
 print("안녕하세요.\n만나서\t\t반갑습니다.")
+
+#6. print 함수에 두 개의 단어를 입력한 예제
+print("오늘은", "일요일")
+
+#7. naver;kakao;sk;samsung 출력
+print("naver","kakao","sk;samsung", sep=";")
+
+#8. naver/kakao/sk/samsung 출력
+print("naver","kakao","sk","samsung", sep="/")
+
+#9. print("first");print("second") 수정하여 줄바꿈이 없이 출력, 세미콜론 (;)은 한줄에 여러 개의 명령을 작성하기 위해 사용
+print("first",end=""); print("second")
+
+#10. 5/3의 결과를 화면에 출력
+print(5/3)
