095380c645f11932567386a125ce03c199b76af5
adrenalin1004 <63244835+adrenalin1004@users.noreply.github.com>
2021-03-01 01:25:50 +0900

    Create Quiz-210301.py

---
+++ PyQuiz/Quiz-210301.py
@@ -0,0 +1,16 @@
+#1. 화면에 Hello World 문자열을 출력하세요
+print('Hello World')
+
+#2. 화면에 Mary's cosmetics을 출력하세요. (중간에 '가 있음에 주의하세요)
+print('Mary\'s cosmetics')
+print("Mary's cosmetics")
+
+#3. 신씨가 소리질렀다. "도둑이야". 출력
+print('신씨가 소리질렀다. \"도둑이야\".')
+print('신씨가 소리질렀다. "도둑이야".')
+
+#4. 화면에 "C:\Windows"를 출력하세요.
+print('"C:\windows"')
+
+#5. \t와 \n의 역할을 설명, \n 줄바꿈, \t 탭
+print("안녕하세요.\n만나서\t\t반갑습니다.")
