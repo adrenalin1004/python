64a15570e2536401b8caa43bf80f2dc6d19af390
adrenalin1004 <63244835+adrenalin1004@users.noreply.github.com>
2021-03-02 22:35:53 +0900

    Update Quiz-210301.py

--- PyQuiz/Quiz-210301.py
+++ PyQuiz/Quiz-210301.py
@@ -181,3 +181,44 @@ data = "   삼성전자    "
 #공백제거 = data.replace("   ",'')
 공백제거 = data.strip()
 print(공백제거)
+
+#41. upper 메서드, 대문자 BTC_KRW로 변경
+ticker = "btc_krw"
+print(ticker.upper())
+
+#42. lower 메서드, 소문자 btc_krw로 변경
+ticker = "BTC_KRW"
+ticker1 = ticker.lower()
+print(ticker1)
+
+#43. capitalize 메서드, 문자열 'hello'가 있을 때 이를 'Hello'로 변경
+A = 'hello'
+print(A.capitalize())
+
+#44. endswith 메서드, 파일 이름이 'xlsx'로 끝나는지 확인
+file_name = "보고서.xlsx"
+print(file_name.endswith('.xlsx'))
+
+#45. endswith 메서드, 파일 이름이 'xlsx' 또는 'xls'로 끝나는지 확인
+file_name = "보고서.xlsx"
+print(file_name.endswith(("xlsx","xls")))
+
+#46. startswith 메서드, 파일 이름이 '2020'로 시작하는지 확인
+file_name = "2020_보고서.xlsx"
+print(file_name.startswith('2020'))
+
+#47. split 메서드, 공백을 기준으로 문자열을 나눠보세요
+a = "hello world"
+print(a.split())
+
+#48. split 메서드, btc와 krw로 나눠보세요.
+ticker = "btc_krw"
+print(ticker.split("_"))
+
+#49. split 메서드, 날짜를 표현하는 문자열이 있을 때 연도, 월, 일로 나눠보세요.
+date = "2020-05-01"
+print(date.split("-"))
+
+#50. rstrip 메서드, 문자열의 오른쪽에 공백이 있을 때 이를 제거
+data = "039490     "
+print(data.rstrip())
