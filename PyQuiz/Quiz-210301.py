32b13211209046df18c0ef024297c736f322f8ae
adrenalin1004 <63244835+adrenalin1004@users.noreply.github.com>
2021-03-04 22:49:43 +0900

    Update Quiz-210301.py
    2021-03-04

--- PyQuiz/Quiz-210301.py
+++ PyQuiz/Quiz-210301.py
@@ -222,3 +222,47 @@ print(date.split("-"))
 #50. rstrip 메서드, 문자열의 오른쪽에 공백이 있을 때 이를 제거
 data = "039490     "
 print(data.rstrip())
+
+#51. 리스트 생성, 영화 제목을 movie_rank 이름의 리스트에 저장
+mov = ['닥터 스트레인지','스플릿','럭키']
+print(mov)
+
+#52.리스트에 원소 추가, movie_rank 리스트에 "배트맨"을 추가
+mov.append('배트맨')
+print(mov)
+
+#53. "슈퍼맨"을 "닥터 스트레인지"와 "스플릿" 사이에 추가
+mov.insert(1,"슈퍼맨")
+print(mov)
+
+#54. 리스트에서 '럭키'를 삭제
+del mov[3]
+print(mov)
+
+#55.  '스플릿' 과 '배트맨'을 를 삭제
+del mov[2]
+del mov[2]
+print(mov)
+
+#56. lang1과 lang2의 원소를 모두 갖고 있는 langs 리스트
+lang1 = ["C", "C++", "JAVA"]
+lang2 = ["Python", "Go", "C#"]
+langs = lang1+lang2
+print(langs)
+
+#57. 리스트에서 최댓값과 최솟값을 출력
+nums = [1, 2, 3, 4, 5, 6, 7]
+print("min: ", min(nums), "max: ", max(nums))
+
+#58. 리스트의 합을 출력
+nums = [1, 2, 3, 4, 5]
+print(sum(nums))
+
+#59. 리스트에 저장된 데이터의 개수를 화면에 구하하라.
+cook = ["피자", "김밥", "만두", "양념치킨", "족발", "피자", "김치만두", "쫄면", "쏘세지", "라면", "팥빙수", "김치전"]
+print(len(cook))
+
+#60. 리스트의 평균
+nums = [1, 2, 3, 4, 5]
+average = sum(nums)/len(nums)
+print(average)
