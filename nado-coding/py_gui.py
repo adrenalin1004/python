91559400beb50065e65d15110bc83f7cdecef473
adrenalin1004 <63244835+adrenalin1004@users.noreply.github.com>
2021-03-18 08:56:02 +0900

    Update py_gui.py
    메시지박스

--- nado-coding/py_gui.py
+++ nado-coding/py_gui.py
@@ -1,11 +1,12 @@
 from tkinter import *
-from PIL import ImageTk #jpg 이미지를 불러올때
+#from PIL import ImageTk #jpg 이미지를 불러올때
 import tkinter.ttk as ttk
 import time
+import tkinter.messagebox as msgbox
 
 root = Tk()
 root.title("Python GUI")
-root.geometry("640x800+100+100") #가로*세로 + x좌표 + y좌표
+root.geometry("640x1000+100+100") #가로*세로 + x좌표 + y좌표
 #root.resizable(True, False) #x(너비), y(높이) 크기 변경 여부
 
 # --- 버튼----------------------------------------
@@ -183,4 +184,42 @@ menu.add_cascade(label="View", menu=menu_view)
 root.config(menu=menu)
 
 
+# --- 메세지박스, ----------------------------------------
+Label(root, text="----------------메시지박스--------------").pack()
+def info():
+    msgbox.showinfo("알림","정상적으로 예매 완료되었습니다.")
+
+def warn():
+    msgbox.showwarning("경고","모두 매진 되었습니다.")
+
+def error():
+    msgbox.showerror("에러", "결재 에러가 발생했습니다.")
+
+def okcancel():
+    msgbox.askokcancel("확인/취소", "해당 좌석은 유아동반석입니다. 예매하시겠습니까?")
+
+def retrycancel():
+    msgbox.askretrycancel("재시도", "일시적인 오류입니다. 다시하시겠습니까?")
+
+def yesno():
+    msgbox.askyesno("예/아니오", "해당 좌석은 역방향입니다. 예매하시겠습니까?")
+
+def yesnocancel():
+    response = msgbox.askyesnocancel(title=None, message="예매 내역이 저장되지 않았습니다. \n 저장하시겠습니까?")
+    print("응답:", response)
+    if response == 1:
+        print("재시도")
+    elif response == 0:
+        print("취소")
+    else:
+        print("뭘까")
+        
+Button(root, command=info, text="알림").pack()
+Button(root, command=warn, text="경고").pack()
+Button(root, command=error, text="에러").pack()
+
+Button(root, command=okcancel, text="알림").pack()
+Button(root, command=retrycancel, text="재시도 취소").pack()
+Button(root, command=yesno, text="예 아니오").pack()
+Button(root, command=yesnocancel, text="예 아니오 취소").pack()
 root.mainloop()
