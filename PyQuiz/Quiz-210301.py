629f858dd49c04c5b306062708abc5f01678d280
adrenalin1004 <63244835+adrenalin1004@users.noreply.github.com>
2021-03-22 23:13:08 +0900

    Update Quiz-210301.py
    함수정의, #220

--- PyQuiz/Quiz-210301.py
+++ PyQuiz/Quiz-210301.py
@@ -1114,11 +1114,156 @@ volatility = []
 for i in ohlc[1:] :
     if i[3] > i[0]:
        volatility.append(i[1]-i[2])
-       print(volatility)
+       print("#199 : ", volatility)
 
 #200. 시가에 매수해서 종가에 매도 했을 경우 총 수익금을 계산하라.
 val = 0
 for i in ohlc[1:] :
     #val = i[3] - i[0]
     val += (i[3] - i[0])
-    print(val)
+print("#200 : ",val)
+
+#201. "비트코인" 문자열을 화면에 출력하는 print_coin() 함수를 정의하라.
+def print_coin():
+    print("#201: 비트코인")
+
+#202. 201번에서 정의한 함수를 호출하라.
+print_coin()
+
+#203. 201번에서 정의한 print_coin 함수를 100번호출하라.
+for i in range(10) :
+    print_coin()
+
+#204. "비트코인" 문자열을 100번 화면에 출력하는 print_coins() 함수를 정의하라.
+def print_coins():
+    for i in range(10) :
+        print("204: 비트코인")
+
+#205. 아래의 에러가 발생하는 이유에 대해 설명하라.
+#hello()
+def hello():
+    print("Hi")
+
+#206. 아래 코드의 실행 결과를 예측하라.
+def message() :
+    print("A")
+    print("B")
+
+message() #A, B
+print("C") # C
+message() #A, B
+
+#207. 아래 코드의 실행 결과를 예측하라. (읽기 어려운 코드의 예입니다.)
+print("#207--------------------------------")
+print("A")      #A
+def message() :
+    print("B")
+print("C") #C
+message()  #B
+
+#208. 아래 코드의 실행 결과를 예측하라. (읽기 어려운 코드의 예입니다.)
+print("#208--------------------------------")
+print("A")     #A
+def message1() :
+    print("B")
+print("C")     #c
+def message2() :
+    print("D")
+message1()     #B
+print("E")     #E
+message2()     #D
+
+#209. 아래 코드의 실행 결과를 예측하라.
+print("#209--------------------------------")
+def message1():
+    print("A")
+def message2():
+    print("B")
+    message1()
+message2()      #B, A
+
+#210. 아래 코드의 실행 결과를 예측하라.
+print("#210--------------------------------")
+def message1():
+    print("A")
+def message2():
+    print("B")
+def message3():
+    for i in range (3) :
+        message2()
+        print("C")
+    message1()
+message3() #BCBCBC A
+
+#211. 함수의 호출 결과를 예측하라.
+print("#211--------------------------------")
+def 함수(문자열) :
+    print(문자열)
+함수("안녕") #안녕
+함수("Hi")   #Hi
+
+#212. 함수의 호출 결과를 예측하라.
+print("#212--------------------------------")
+def 함수(a, b) :
+    print(a + b)
+함수(3, 4) #7
+함수(7, 8) #15
+
+#213. 아래와 같은 에러가 발생하는 원인을 설명하라.
+def 함수(문자열) :
+    print(문자열)
+#함수() #TypeError: 함수() missing 1 required positional argument: '문자열'
+
+#214. 아래와 같은 에러가 발생하는 원인을 설명하라.
+def 함수(a, b) :
+    print(a + b)
+#함수("안녕", 3) #TypeError: must be str, not int
+
+#215. 하나의 문자를 입력받아 문자열 끝에 ":D" 스마일 문자열을 이어 붙여 출력하는 print_with_smile 함수를 정의하라.
+def print_with_simle(string):
+    print(string,":D")
+
+#216. 215에서 정의한 함수를 호출하라. 파라미터는 "안녕하세요"로 입력하라.
+print("#216--------------------------------")
+print_with_simle("안녕하세요.")
+
+#217. 현재 가격을 입력 받아 상한가 (30%)를 출력하는 print_upper_price 함수를 정의하라
+# def print_upper_price():
+#     print(int(input("#217. 현재가를 입력하시오: "))*1.3)
+# print_upper_price()
+print("#217--------------------------------")
+def print_upper_price(price) :
+    print(price * 1.3)
+print_upper_price(1000)
+
+#218. 두 개의 숫자를 입력받아 두 수의 합을 출력하는 print_sum 함수를 정의하라.
+print("#218--------------------------------")
+def print_sum(a, b):
+    print("#218 %d + %d = %d" %(a,b,a+b))
+print_sum(3,4)
+
+#219. 두 개의 숫자를 입력받아 합/차/곱/나눗셈을 출력하는 print_arithmetic_operation 함수를 작성하라.
+print("#219--------------------------------")
+def print_arithmetic_operation(a, b):
+    print(a + b)
+    print(a - b)
+    print(a*b)
+    print(a/b)
+print_arithmetic_operation(4, 4)
+
+#220. 세 개의 숫자를 입력받아 가장 큰수를 출력하는 print_max 함수를 정의하라. 단 if 문을 사용해서 수를 비교하라.
+print("#220--------------------------------")
+def print_max(a,b,c):
+    if a > b :
+        if a > c :
+            print(a)
+        else:
+            print(c)
+    elif b > a :
+        if b > c :
+            print(b)
+        else:
+            print(c)
+    else:
+        print(c)
+print_max(1,2,3)
