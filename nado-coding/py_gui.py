eaa16db8c9bb17d44b3a1db56865d5273ebef623
adrenalin1004 <63244835+adrenalin1004@users.noreply.github.com>
2021-03-24 08:48:43 +0900

    Update py_gui.py
    python gui 프레임, 스크롤, 그리드

--- nado-coding/py_gui.py
+++ nado-coding/py_gui.py
@@ -9,6 +9,16 @@ root.title("Python GUI")
 root.geometry("640x1000+100+100") #가로*세로 + x좌표 + y좌표
 #root.resizable(True, False) #x(너비), y(높이) 크기 변경 여부
 
+'''
+
+# scrollbar = Scrollbar(root)
+# scrollbar.pack(side="right", fill="y")
+# mylist=Listbox(root,yscrollcommand=scrollbar.set)
+# for line in range(50):
+#     mylist.insert(END, str(line))
+# mylist.pack(side=LEFT,fill=BOTH)
+# scrollbar.config(command=mylist.yview)
+
 # --- 버튼----------------------------------------
 # btn1 = Button(root, text = "버튼1")
 # btn1.pack()
@@ -222,4 +232,101 @@ Button(root, command=okcancel, text="알림").pack()
 Button(root, command=retrycancel, text="재시도 취소").pack()
 Button(root, command=yesno, text="예 아니오").pack()
 Button(root, command=yesnocancel, text="예 아니오 취소").pack()
+
+# --- 프레임박스, ----------------------------------------
+Label(root, text="메뉴를 선택해 주세요.").pack(side="top")
+Label(root, text="주문하기").pack(side="bottom")
+
+Label(root, text="----------------프레임--------------").pack()
+
+frame_burger = Frame(root, relief="solid", bd=1)
+frame_burger.pack(side="left", fill="both", expand=True)
+
+Button(frame_burger, text="햄버거").pack()
+Button(frame_burger, text="치즈버거").pack()
+Button(frame_burger, text="치킨버거").pack()
+
+frame_drink = LabelFrame(root, text = "음료")
+frame_drink.pack(side="right", fill="both", expand=True)
+Button(frame_drink, text="사이다").pack()
+Button(frame_drink, text="콜라").pack()
+
+
+# --- 스크롤, ----------------------------------------
+frame = Frame(root)
+frame.pack()
+
+scrollbar = Scrollbar(frame)
+scrollbar.pack(side="right", fill="y")
+listbox = Listbox(frame, selectmode="extended", height=10, yscrollcommand=scrollbar.set )
+for i in range(1, 32):
+    listbox.insert(END, str(i) + "일")
+listbox.pack(side="left")
+
+scrollbar.config(command=listbox.yview)
+
+'''
+
+# --- 그리드, ----------------------------------------
+#btn1 = Button(root, text="버튼1")
+#btn2 = Button(root, text="버튼2")
+# btn1.pack(side="left")
+# btn2.pack(side="right")
+
+btn_f16 = Button(root, text="F16", width=5, height=2)
+btn_f17 = Button(root, text="F17", width=5, height=2)
+btn_f18 = Button(root, text="F18", width=5, height=2)
+btn_f19 = Button(root, text="F19", width=5, height=2)
+
+btn_f16.grid(row=0, column=0, sticky=N+E+W+S, padx=2, pady=2)
+btn_f17.grid(row=0, column=1, sticky=N+E+W+S, padx=2, pady=2)
+btn_f18.grid(row=0, column=2, sticky=N+E+W+S, padx=2, pady=2)
+btn_f19.grid(row=0, column=3, sticky=N+E+W+S, padx=2, pady=2)
+
+btn_clear = Button(root, text="clear", width=5, height=2)
+btn_equal = Button(root, text="=", width=5, height=2)
+btn_div = Button(root, text="/", width=5, height=2)
+btn_mul = Button(root, text="*", width=5, height=2)
+
+btn_clear.grid(row=1, column=0, sticky=N+E+W+S, padx=2, pady=2)
+btn_equal.grid(row=1, column=1, sticky=N+E+W+S, padx=2, pady=2)
+btn_div.grid(row=1, column=2, sticky=N+E+W+S, padx=2, pady=2)
+btn_mul.grid(row=1, column=3, sticky=N+E+W+S, padx=2, pady=2)
+
+btn_7 = Button(root, text="7", width=5, height=2)
+btn_8 = Button(root, text="8", width=5, height=2)
+btn_9 = Button(root, text="9", width=5, height=2)
+btn_sub = Button(root, text="sub", width=5, height=2)
+
+btn_7.grid(row=2, column=0, sticky=N+E+W+S, padx=2, pady=2)
+btn_8.grid(row=2, column=1, sticky=N+E+W+S, padx=2, pady=2)
+btn_9.grid(row=2, column=2, sticky=N+E+W+S, padx=2, pady=2)
+btn_sub.grid(row=2, column=3, sticky=N+E+W+S, padx=2, pady=2)
+
+btn_4 = Button(root, text="4", width=5, height=2)
+btn_5 = Button(root, text="5", width=5, height=2)
+btn_6 = Button(root, text="6", width=5, height=2)
+btn_add = Button(root, text="add", width=5, height=2)
+
+btn_4.grid(row=3, column=0, sticky=N+E+W+S, padx=2, pady=2)
+btn_5.grid(row=3, column=1, sticky=N+E+W+S, padx=2, pady=2)
+btn_6.grid(row=3, column=2, sticky=N+E+W+S, padx=2, pady=2)
+btn_add.grid(row=3, column=3, sticky=N+E+W+S, padx=2, pady=2)
+
+btn_1 = Button(root, text="1", width=5, height=2)
+btn_2 = Button(root, text="2", width=5, height=2)
+btn_3 = Button(root, text="3", width=5, height=2)
+btn_enter = Button(root, text="enter", width=5, height=2)
+
+btn_1.grid(row=4, column=0, sticky=N+E+W+S, padx=2, pady=2)
+btn_2.grid(row=4, column=1, sticky=N+E+W+S, padx=2, pady=2)
+btn_3.grid(row=4, column=2, sticky=N+E+W+S, padx=2, pady=2)
+btn_enter.grid(row=4, column=3, rowspan=2, sticky=N+E+W+S, padx=2, pady=2)
+
+btn_0 = Button(root, text="0", width=5, height=2)
+btn_point = Button(root, text=".", width=5, height=2)
+
+btn_0.grid(row=5, column=0, columnspan=2, sticky=N+E+W+S, padx=2, pady=2)
+btn_point.grid(row=5, column=2, sticky=N+E+W+S, padx=2, pady=2)
+
 root.mainloop()
