d0e1a14f9ec4499c3ad50351b10dea47b0212f82
adrenalin1004 <63244835+adrenalin1004@users.noreply.github.com>
2021-03-29 22:13:24 +0900

    Update Quiz-210301.py
    함수, #240

--- PyQuiz/Quiz-210301.py
+++ PyQuiz/Quiz-210301.py
@@ -1267,3 +1267,168 @@ def print_max(a,b,c):
     else:
         print(c)
 print_max(1,2,3)
+
+#221. 입력된 문자열을 역순으로 출력하는 print_reverse 함수를 정의하라.
+print("#221--------------------------------")
+def print_reverse(string) :
+    print(string[::-1])
+print_reverse("python")
+
+#222. 성적 리스트를 입력 받아 평균을 출력하는 print_score 함수를 정의하라.
+print("#222--------------------------------")
+# def print_score(num1,num2,num3) :
+#     sum = (num1+num2+num3)/3
+#     print(sum)
+def print_score(score_list) :
+    print(sum(score_list)/len(score_list))
+print_score([1,2,3])
+
+#223. 하나의 리스트를 입력받아 짝수만 화면에 출력하는 print_even 함수를 정의하라.
+print("#223--------------------------------")
+def print_even(list_even):
+    for i in list_even :
+        if i%2 == 0:
+            print(i)
+print_even ([1, 3, 2, 10, 12, 11, 15])
+
+#224. 하나의 딕셔너리를 입력받아 딕셔너리의 key 값을 화면에 출력하는 print_keys 함수를 정의하라.
+print("#224--------------------------------------------------")
+def print_keys(key):
+    for keys in key.keys():
+        print(keys)
+print_keys ({"이름":"김말똥", "나이":30, "성별":0})
+
+#225. my_dict와 날짜 키값을 입력받아 OHLC 리스트를 출력하는 print_value_by_key 함수를 정의하라.
+print("#225------------------------")
+my_dict = {"10/26" : [100, 130, 100, 100],
+           "10/27" : [10, 12, 10, 11]}
+def print_value_by_key(dics, values):
+    print(dics[values])
+print_value_by_key  (my_dict, "10/26")
+
+#226. 입력 문자열을 한 줄에 다섯글자씩 출력하는 print_5xn(string) 함수를 작성하라.
+print("#226------------------------")
+def print_5xn(string):
+    print(string[:5]+"\n"+string[5:])
+print_5xn("아이엠어보이유알어걸")
+
+#227. 문자열과 한줄에 출력될 글자 수를 입력을 받아 한 줄에 입력된 글자 수만큼 출력하는 print_mxn(string) 함수를 작성하라.
+print("#227----------------------------")
+def print_mxn(string, value):
+    lens = int(len(string)/3)
+    for i in range(lens+1):
+        print(i)
+        print(string[i*value: i*value+value] )
+print_mxn("아이엠어보이유알어걸", 3)
+
+#228. 연봉을 입력받아 월급을 계산하는 calc_monthly_salary(annual_salary) 함수를 정의하라. 회사는 연봉을 12개월로 나누어 분할 지급하며, 이 때 1원 미만은 버림한다.
+print("#228----------------------------")
+def calc_monthly_salary(salary):
+    m_salary = int(salary/12)
+    print(m_salary)
+calc_monthly_salary(12000000)
+
+#229. 아래 코드의 실행 결과를 예측하라.
+print("#229----------------------------")
+def my_print (a, b) :
+    print("왼쪽:", a)
+    print("오른쪽:", b)
+my_print(a=100, b=200)  # 왼쪽: 100, 오른쪽: 200
+
+#230. 아래 코드의 실행 결과를 예측하라.
+print("#230----------------------------")
+def my_print (a, b) :
+    print("왼쪽:", a)
+    print("오른쪽:", b)
+my_print(b=100, a=200) #왼쪽 200, 오른쪽 100
+
+#231. 아래 코드의 실행 결과를 예측하라.
+print("#231----------------------------")
+def n_plus_1 (n) :
+    result = n + 1
+n_plus_1(3)
+#print (result) #함수 밖에서 호출 불가
+
+#232. 문자열 하나를 입력받아 인터넷 주소를 반환하는 make_url 함수를 정의하라.
+print("#232----------------------------")
+def make_url(url):
+    domain = "www."+url+".com"
+    print(domain)
+make_url("naver")
+#www.naver.com
+
+#233. 문자열을 입력받아 각 문자들로 구성된 리스트로 반환하는 make_list 함수를 정의하라.
+print("#233----------------------------")
+def make_list(str):
+    #print(list(str))
+    list = []
+    for i in str :
+        list.append(i)
+    #print(list)
+    return list
+make_list("abcd")
+
+#234.숫자로 구성된 하나의 리스트를 입력받아, 짝수들을 추출하여 리스트로 반환하는 pickup_even 함수를 구현하라.
+print("#234----------------------------")
+def pickup_even(val):
+    list = []
+    for i in val:
+        if i%2 == 0:
+            list.append(i)
+    print(list)
+pickup_even([3, 4, 5, 6, 7, 8])
+
+#235. 콤마가 포함된 문자열 숫자를 입력받아 정수로 변환하는 convert_int 함수를 정의하라.
+print("#235----------------------------")
+def convert_int(value):
+    conv = int(value.replace(',',''))
+    print(conv)
+convert_int("1,234,567")
+
+#236. 아래 코드의 실행 결과를 예측하라.
+print("#236----------------------------")
+def 함수(num) :
+    return num + 4
+a = 함수(10)
+b = 함수(a)
+c = 함수(b)
+print(c)  #22
+
+#237. 아래 코드의 실행 결과를 예측하라.
+print("#237----------------------------")
+def 함수(num) :
+    return num + 4
+c = 함수(함수(함수(10)))
+print(c)  #22
+
+#238. 아래 코드의 실행 결과를 예측하라.
+print("#238----------------------------")
+def 함수1(num) :
+    return num + 4
+def 함수2(num) :
+    return num * 10
+a = 함수1(10) #14
+c = 함수2(a) #140
+print(c) #140
+
+#239. 아래 코드의 실행 결과를 예측하라.
+print("#239----------------------------")
+def 함수1(num) :
+    return num + 4
+def 함수2(num) :
+    num = num + 2
+    return 함수1(num)
+c = 함수2(10) #16
+print(c)
+
+#240. 아래 코드의 실행 결과를 예측하라.
+print("#240----------------------------")
+def 함수0(num) :
+    return num * 2
+def 함수1(num) :
+    return 함수0(num + 2)
+def 함수2(num) :
+    num = num + 10
+    return 함수1(num)
+c = 함수2(2) #28
+print(c)
