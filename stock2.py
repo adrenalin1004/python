f060e77b26de801a19ad2d321a5b7f61ca7a453a
adrenalin1004 <63244835+adrenalin1004@users.noreply.github.com>
2021-04-05 17:22:24 +0900

    Update stock2.py
    실시간조회 비교

--- stock2.py
+++ stock2.py
@@ -5,7 +5,8 @@ from datetime import datetime
 import time
 
 print("---stock2--------------")
-r = requests.get("http://wap.futurewiz.co.kr/dragon/stockgame_rt/starwars202103/detail_transaction/H0000469?charge=yes")
+#r = requests.get("http://wap.futurewiz.co.kr/dragon/stockgame_rt/starwars202103/detail_transaction/H0000469?charge=yes")
+r = requests.get("http://wap.futurewiz.co.kr/dragon/stockgame_rt/starwars202103/detail_transaction/H0000472?charge=yes")
 soup = BeautifulSoup(r.content.decode('euc-kr', 'replace'), "html.parser")
 items = soup.select("#mainCont > div > table > tbody") #td.lft span.crUp
 
@@ -19,22 +20,50 @@ price = soup.select_one("#mainCont > div > table > tbody > tr:nth-child(2) > td:
 stocklist = []
 
 for item in zip(date, name, sell, stock, price):
-    stocklist.append(
-        {
-            'time' : item[0],
-            'name' : item[1],
-            'sell' : item[2],
-            'stock': item[3],
-            'price': item[4]
-        }
-    )
-
-while True:
     now = datetime.now()
-    print(now)
-    print(stocklist[0])
-    print("---새로고침--------------")
-    time.sleep(10)
+    format_date = now.strftime("%m/%d")
+    # stocklist.append(
+    #     {
+    #         'time' : item[0],
+    #         'name' : item[1],
+    #         'sell' : item[2],
+    #         'stock': item[3],
+    #         'price': item[4]
+    #     }
+    # )
+    stocklist.append(date)
+    stocklist.append(name)
+    stocklist.append(sell)
+    stocklist.append(stock)
+    stocklist.append(price)
+    date_diff = stocklist[0].string
+    date_diff_con = date_diff.split(" ")
+    date_int = date_diff_con[0]
+    # print(date_int, type(date_int))
+    # print(format_date, type(format_date))
+    # print(date_int == format_date)
+
+    if date_int == format_date :
+        while True:
+            now = datetime.now()
+            print(now)
+            print(stocklist[0].string)
+            print(stocklist[1].string)
+            print(stocklist[2].string)
+            print(stocklist[3].string)
+            print(stocklist[4].string)
+            print("---새로고침--------------\n")
+            time.sleep(10)
+    else:
+        print("꽝")
+
+# while True:
+#     now = datetime.now()
+#     print(now)
+#     print(stocklist['time'])
+#     print(stocklist[0])
+#     print("---새로고침--------------\n")
+#     time.sleep(100)
 
 '''
 for item in items:
