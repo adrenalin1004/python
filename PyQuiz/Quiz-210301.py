c2e7fcb8d52391ea7bbe2cd404d1fae77dbe6f7f
adrenalin1004 <63244835+adrenalin1004@users.noreply.github.com>
2021-04-27 23:40:52 +0900

    Update Quiz-210301.py
    Account 클래스 #280

--- PyQuiz/Quiz-210301.py
+++ PyQuiz/Quiz-210301.py
@@ -1098,7 +1098,7 @@ for i in ohlc[1:] :
     if i[3] >= i[0] :
         print(i[3])
  
-""" # End of Code
+
 #198. ohlc 리스트에는 시가(open), 고가 (high), 저가 (low) , 종가(close)가 날짜별로 저장돼 있다. 고가와 저가의 차이를 변동폭으로 정의할 때 변동폭을 volatility 이름의 리스트에 저장하라.
 ohlc = [["open", "high", "low", "close"],
         [100, 110, 70, 100],
@@ -1562,7 +1562,7 @@ ret.setInfo("조아람", 25, "여자")
 ret.who()
 
 #259 클래스 소멸자, 사람 (human) 클래스에 "나의 죽음을 알리지 말라"를 출력하는 소멸자를 추가하세요.
-del(Human)
+#del(Human)
 
 #260 에러의 원인,아래와 같은 에러가 발생한 원인에 대해 설명하세요.
 # class OMG : 
@@ -1709,8 +1709,8 @@ for i in list:
 #은행이름: SC은행
 #계좌번호: 111-11-111111
 
+
 import random
-'''
 class Account:
     def __init__(self, name, balance):
        self.name = name
@@ -1732,6 +1732,7 @@ print(kim.bank)
 print(kim.account_number)
 '''
 #272 클래스 변수, 클래스 변수를 사용해서 Account 클래스로부터 생성된 계좌 객체의 개수를 저장하세요.
+'''
 class Account:
     account_count = 0
 
@@ -1755,12 +1756,20 @@ user = Account('홍길동', 1000)
 print(Account.account_count)
 lee = Account("이민수", 100)
 print(Account.account_count)
+""" # End of Code
+
 
 #273 클래스 변수 출력, Account 클래스로부터 생성된 계좌의 개수를 출력하는 get_account_num() 메서드를 추가하세요.
-class Account:
+import random
+
+class Account1:
     account_count = 0
 
     def __init__(self, name, balance):
+        self.deposit_count = 0
+        self.deposit_log = []
+        self.withdraw_log = []
+
         self.name = name
         self.balance = balance
         self.bank = 'KB은행'
@@ -1773,30 +1782,100 @@ class Account:
         num2 = str(num2).zfill(2)
         num3 = str(num3).zfill(6)
         self.account_number = num1 + '-' + num2 + '-' + num3
-
-        Account.account_count += 1
+        Account1.account_count += 1
     
     def get_account_num(cls):
         print(cls.account_count)
     
     def deposit(self, amount):
-        print("deposit")
         if amount >= 1:
             self.balance += amount
+
+            self.deposit_count += 1
+            if self.deposit_count % 5 == 0:
+                self.balance = (self.balance * 1.01)
 
     def withdraw(self, amount):
         if self.balance > amount:
             self.balance -= amount
 
-user = Account('홍길동', 1000)
-user2 = Account('홍길동2', 100)
-user3 = Account('홍길동3', 10)
+    def display_info(self):
+        print("은행이름: ", self.bank)
+        print("예금주: ", self.name)
+        print("계좌번호: ", self.account_number)
+        print("잔고: ", self.balance)
+    
+    def withdraw_history(self):
+        for amount in self.withdraw_log:
+            print("withdraw_log")
+            print(amount)
+    def deposit_history(self):
+        for amount in self.deposit_log:
+            print("deposit_log")
+            print(amount)
+
+user = Account1('홍길동', 1000)
+#user2 = Account('홍길동2', 100)
+#user3 = Account('홍길동3', 10)
 #print(Account.account_count)
-user.get_account_num()
+#user.get_account_num()
 
 #274 입금 메서드, Account 클래스에 입금을 위한 deposit 메서드를 추가하세요. 입금은 최소 1원 이상만 가능합니다.
 user.deposit(100)
-
 #275 출금 메서드, Account 클래스에 출금을 위한 withdraw 메서드를 추가하세요. 출금은 계좌의 잔고 이상으로 출금할 수는 없습니다.
-user.withdraw(80)
-print(user.balance)
+#user.withdraw(80)
+#print(user.balance)
+
+#276 정보 출력 메서드, Account 인스턴스에 저장된 정보를 출력하는 display_info() 메서드를 추가하세요. 잔고는 세자리마다 쉼표를 출력하세요.
+'''
+은행이름: SC은행
+예금주: 파이썬
+계좌번호: 111-11-111111
+잔고: 10,000원
+
+
+p = Account("파이썬", 10000)
+p.display_info()
+'''
+
+#277 이자 지급하기, 입금 횟수가 5회가 될 때 잔고를 기준으로 1%의 이자가 잔고에 추가되도록 코드를 변경해보세요.
+p = Account1("파이썬2", 10000)
+p.deposit(10000)
+p.deposit(10000)
+p.deposit(10000)
+p.deposit(5000)
+p.deposit(5000)
+print(p.balance)
+p.deposit_history()
+
+#278 여러 객체 생성
+#Account 클래스로부터 3개 이상 인스턴스를 생성하고 생성된 인스턴스를 리스트에 저장해보세요.
+
+data = []
+k = Account1("KIM", 90000000)
+l = Account1("LEE", 10000)
+p = Account1("PARK", 10000)
+
+data.append(k)
+data.append(l)
+data.append(p)
+
+#279 객체 순회
+#반복문을 통해 리스트에 있는 객체를 순회하면서 잔고가 100만원 이상인 고객의 정보만 출력하세요.
+print("---#279-------------------")
+for c in data:
+    if c.balance >= 1000000:
+        c.display_info()
+
+#280 입출금 내역
+#입금과 출금 내역이 기록되도록 코드를 업데이트 하세요. 입금 내역과 출금 내역을 출력하는 deposit_history와 withdraw_history 메서드를 추가하세요.
+print("---#280-------------------")
+pjh = Account1("Kim", 1000)
+pjh.deposit(100)
+pjh.deposit(200)
+pjh.deposit(300)
+pjh.deposit_history()
+
+pjh.withdraw(100)
+pjh.withdraw(200)
+pjh.withdraw_history()
